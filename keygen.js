47019db1abd6c6fc707a91b119012fce0b5b3230
Axat Bhardwaj <axatbhardwaj@outlook.com>
2021-09-01 19:54:31 +0530

    Keygen

--- keygen.js
+++ keygen.js
@@ -1,14 +1,17 @@
-const crypto = require('crypto')
-const x = crypto.generateKeyPairSync("rsa", {
+function Keygen() {
+  const crypto = require('crypto');
+  const x = crypto.generateKeyPairSync('rsa', {
     modulusLength: 530, // options
     publicExponent: 0x10101,
     publicKeyEncoding: {
-        type: 'pkcs1',
-        format: 'der'
+      type: 'pkcs1',
+      format: 'der',
     },
     privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'der',
-    }
-
-});
+      type: 'pkcs8',
+      format: 'der',
+    },
+  });
+  return x;
+}
+module.exports = Keygen;
